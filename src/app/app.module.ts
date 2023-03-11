34de8b5e68196eb3cf96bbb4cc5f26018fffd666
Alef1412 <102564597+Alef1412@users.noreply.github.com>
2023-03-11 01:34:34 -0300

    add  CSS style in header and add bootstrap on the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +22,6 @@ import { BooksComponent } from './components/books/books.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
